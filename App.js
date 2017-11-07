d4b253412f753ef9b38b0cdcb643c9da2b78e770
Osayame Gaius-Obaseki <osayame@gmail.com>
2017-11-06 22:05:53 -0500

    completed rough geolocation with button rendering

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Text, View, StyleSheet, TextInput, Alert, Image, Button } from 'react-native';
 import Firebase from 'firebase';
+import { Location, Permissions } from 'expo';
+import GeoPoint from 'geopoint';
 
 const config = {
     apiKey: "AIzaSyBtvhszPNtcL-o8qa9vNUIY8FCLuN_FyUI",
@@ -134,11 +136,63 @@ const styles = StyleSheet.create({
   },
 });
 
-const HomeScreen = () => (
-  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-    <Text>Home Screen</Text>
-  </View>
-);
+export class HomeScreen extends Component {
+  state = {
+    location: {}
+  };
+
+  componentWillMount() {
+    this._getLocationAsync();
+  }
+
+  _getLocationAsync = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+
+    if (status !== 'granted') {
+      this.setState({
+        errorMessage: 'Permission to access location was denied',
+      });
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+    this.setState({ location });
+  };
+
+  locationIsValid() {
+    const TNS_LAT = 33.746416;
+    const TNS_LONG = -84.365960;
+    const tnsLocation = new GeoPoint(TNS_LAT, TNS_LONG);
+    const MAX_DISTANCE = 3.1;
+
+    let {location} = this.state;
+    if (location && location.coords) {
+      let {coords} = location;
+      let currentPoint = new GeoPoint(coords.latitude, coords.longitude);
+
+      if (tnsLocation.distanceTo(currentPoint) < MAX_DISTANCE) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  renderButton() {
+    if (this.locationIsValid()) {
+      return <Button title="Check In"></Button>;
+    } else {
+      return <Text>Oops! Looks like you're not on campus. Try again later.</Text>;
+    }
+  }
+
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        {this.renderButton()}
+      </View>
+
+    )
+  }
+}
 
 const RootNavigator = StackNavigator({
   Login: {
